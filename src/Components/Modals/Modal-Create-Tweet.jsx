20921ff91c52d79bcc99bd3bdcaeee6a68564e0b
Suji Falcon <suji.falcon@gmail.com>
2022-12-02 09:23:53 -0500

    CHARACTER COUNTER ON THE TWEEETBOX WAHOOOO

--- src/Components/Modals/Modal-Create-Tweet.jsx
+++ src/Components/Modals/Modal-Create-Tweet.jsx
@@ -1,7 +1,7 @@
 import "./Modal.css";
 // import { TextInput } from 'react-native';
 import React, { useState } from "react";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 import { createPost } from "../../services/PostCrud";
 
 function ModalCreateTweet(props) {
@@ -13,20 +13,20 @@ function ModalCreateTweet(props) {
 		setCharacterCount(event.target.value.length);
 	}
 
-  async function doSubmit(event){
-    event.preventDefault()
-    try {
-      const content = {
-        text: tweet,
-        title: "Text",
-      }
-      await createPost(content)
-      window.location.reload()
-    } catch (error) {
-      throw error;
-    }
-    return props.onClose;
-  };
+	async function doSubmit(event) {
+		event.preventDefault();
+		try {
+			const content = {
+				text: tweet,
+				title: "Text",
+			};
+			await createPost(content);
+			window.location.reload();
+		} catch (error) {
+			throw error;
+		}
+		return props.onClose;
+	}
 
 	if (!props.show) {
 		return null;
@@ -37,29 +37,26 @@ function ModalCreateTweet(props) {
 				<div className="modal-header">
 					<h1>@{Cookies.get("User")}</h1>
 				</div>
-        <div className="modal-body">
-          <textarea
-            className="text-area"
-            id="tweetMessage"
-            name="tweetMessage"
-            placeholder="What's Happening?"
-            maxLength="280"
-            onChange={handleChange}>
-          </textarea>
-          <br></br>
-          <textarea
-            className="text-area"
-            id="tweetImage"
-            name="tweetImage"
-            placeholder="Input ImageURL"
-            maxLength="280"
-            onChange={handleChange}>
-          </textarea>
-          
+				<div className="modal-body">
+					<textarea
+						className="text-area"
+						id="tweetMessage"
+						name="tweetMessage"
+						placeholder="What's Happening?"
+						maxLength="280"
+						onChange={handleChange}></textarea>
+					<br></br>
+					<textarea
+						className="text-area"
+						id="tweetImage"
+						name="tweetImage"
+						placeholder="Input ImageURL"
+						maxLength="280"
+						onChange={handleChange}></textarea>
 				</div>
 				<div className="modal-footer">
 					<p className="character-counter">{characterCount}/280</p>
-          <button className="modal-button" onClick={doSubmit}>
+					<button className="modal-button" onClick={doSubmit}>
 						Post
 					</button>
 				</div>
